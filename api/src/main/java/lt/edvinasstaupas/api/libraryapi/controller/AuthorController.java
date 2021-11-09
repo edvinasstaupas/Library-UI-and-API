4356b768d1830069563e78abcb8f9b6e87ca39ca
edvinasstaupas <staupas.edvinas@gmail.com>
2021-11-09 13:35:09 +0200

    [API] [BUG] fix misspell in mapping

--- api/src/main/java/lt/edvinasstaupas/api/libraryapi/controller/AuthorController.java
+++ api/src/main/java/lt/edvinasstaupas/api/libraryapi/controller/AuthorController.java
@@ -15,7 +15,7 @@ import static org.springframework.http.ResponseEntity.notFound;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
-@RequestMapping("Author")
+@RequestMapping("author")
 @RequiredArgsConstructor
 public class AuthorController {
 
