305bf4dd6fa119019fd915b56180901f3b75caed
edvinasstaupas <staupas.edvinas@gmail.com>
2021-11-11 18:29:09 +0200

    [API] more fields for user and init data

--- api/src/main/java/lt/edvinasstaupas/api/libraryapi/entity/User.java
+++ api/src/main/java/lt/edvinasstaupas/api/libraryapi/entity/User.java
@@ -5,6 +5,7 @@ import lt.edvinasstaupas.api.libraryapi.service.entity.RoleFactory;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -19,6 +20,21 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "avatar_path")
+    private String avatarPath;
+
+    @OneToOne
+    private Address address;
+
+    @OneToMany(mappedBy = "takenBy")
+    private List<Copy> copies;
 
     @ManyToMany()
     private Set<Role> roles = new HashSet<>();
--- api/src/main/resources/db/data.sql
+++ api/src/main/resources/db/data.sql
@@ -1,6 +1,8 @@
+insert into USERS (id, first_name, last_name) values ( 1, 'Edvinas', 'Staupas' );
 insert into AUTHOR (id, isbn, first_name, last_name)
 values (1, 9781782924142, 'J. K.', 'Rowling');
 insert into BOOK (id, isbn, author_id, title, published_at)
 values (1, 9780439064873, 1, 'Harry Potter and the Chamber of Secrets', CURRENT_DATE);
 insert into LIBRARY (id, name) values ( 1, 'LNB' );
 insert into COPY (id, book_id, library_id, is_taken) values ( 1, 1, 1, false );
+//insert into COPY (id, book_id, library_id, is_taken, takenby) values ( 2, 1, 1, true, 1 );
