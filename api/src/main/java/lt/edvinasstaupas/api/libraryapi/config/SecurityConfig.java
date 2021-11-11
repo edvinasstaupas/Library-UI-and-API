98237a07a56b1776e7a1fd1c95b25abdc136ae93
edvinasstaupas <staupas.edvinas@gmail.com>
2021-11-11 20:31:48 +0200

    [API] remove security for now because it doesnt recognise endpoint

--- api/src/main/java/lt/edvinasstaupas/api/libraryapi/config/SecurityConfig.java
+++ api/src/main/java/lt/edvinasstaupas/api/libraryapi/config/SecurityConfig.java
@@ -1,12 +1,22 @@
 package lt.edvinasstaupas.api.libraryapi.config;
+/*
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @RequiredArgsConstructor
-//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public abstract class SecurityConfig //extends WebSecurityConfigurerAdapter {
-{
-    /*private final UserDetailsService userDetailsService;
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public abstract class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
 
     private final PasswordEncoder passwordEncoder;
 
@@ -16,8 +26,8 @@ public abstract class SecurityConfig //extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/public/**", "/error", "/login", "/register", "/", "/post/**", "/logout").permitAll()
-                .antMatchers("/private/**", "/create-post", "/edit-post", "/edit-comment", "/admin-panel", "/**").authenticated()
+                .antMatchers("/**").permitAll()
+                //.antMatchers("/private/**", "/create-post", "/edit-post", "/edit-comment", "/admin-panel", "/**").authenticated()
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -27,9 +37,8 @@ public abstract class SecurityConfig //extends WebSecurityConfigurerAdapter {
                 //
                 .formLogin()
                 .permitAll()
-                .loginPage("/login")
                 .loginProcessingUrl("/login")
-                .usernameParameter("username")
+                .usernameParameter("userNumber")
                 .passwordParameter("password")
                 .defaultSuccessUrl("/", true)
                 .failureUrl("/login?error")
@@ -54,5 +63,6 @@ public abstract class SecurityConfig //extends WebSecurityConfigurerAdapter {
         auth
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder);
-    }*/
+    }
 }
+*/
