28a77dc8222121af6ae83f1107e3cbb1c5b466ca
edvinasstaupas <staupas.edvinas@gmail.com>
2021-11-20 21:13:11 +0200

    [API] add some data for init db

--- api/src/main/resources/db/data.sql
+++ api/src/main/resources/db/data.sql
@@ -4,7 +4,9 @@ insert into USERS_ROLES values ( 1, 1 );
 insert into AUTHOR (id, isbn, first_name, last_name)
 values (1, 9781782924142, 'J. K.', 'Rowling');
 insert into BOOK (id, isbn, author_id, title, published_at)
-values (1, 9780439064873, 1, 'Harry Potter and the Chamber of Secrets', CURRENT_DATE);
+values (1, 9780439064873, 1, 'Harry Potter and the Chamber of Secrets', CURRENT_DATE),
+       (2, 9780439064874, 1, 'Harry Potter and the Chamber of Secrets (2)', CURRENT_DATE);
 insert into LIBRARY (id, name) values ( 1, 'LNB' );
 insert into COPY (id, book_id, library_id, is_taken) values ( 1, 1, 1, false );
-//insert into COPY (id, book_id, library_id, is_taken, takenby) values ( 2, 1, 1, true, 1 );
+insert into COPY (id, book_id, library_id, is_taken, taken_by_user_id, due_at) values ( 2, 1, 1, true, 1 , CURRENT_DATE);
+insert into COPY (id, book_id, library_id, is_taken, taken_by_user_id, due_at) values ( 3, 2, 1, true, 1 , '2021-12-15');
