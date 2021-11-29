b74aa2d44cdc78b9641a7c6a518f58ef4581b1a5
edvinasstaupas <staupas.edvinas@gmail.com>
2021-11-29 14:06:09 +0200

    [API] add password encoder bean

--- api/src/main/java/lt/edvinasstaupas/api/libraryapi/config/ApplicationConfig.java
+++ api/src/main/java/lt/edvinasstaupas/api/libraryapi/config/ApplicationConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -46,6 +48,11 @@ public class ApplicationConfig implements WebMvcConfigurer {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 /*
     @Bean
