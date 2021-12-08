9c9226d2ea56abbd807826bfe20e1d4b67ce11e1
edvinasstaupas <staupas.edvinas@gmail.com>
2021-12-08 22:38:43 +0200

    [UI] add librarian book add page and user search page

---
+++ ui/src/containers/Pages/LibrarianPages/LibrarianAddBook.js
@@ -0,0 +1,221 @@
+import {useHistory} from "react-router-dom";
+import {useEffect, useState} from "react";
+import 'react-day-picker/lib/style.css';
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import {createBookApi, fetchAuthors} from "../../../api/apiEndpoints";
+import {PrimaryButton} from "../../../components/StyledItems";
+import {Field, Form, Formik} from 'formik';
+import 'moment/locale/en-gb';
+import {
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    Container,
+    FormControl,
+    Grid,
+    InputLabel,
+    MenuItem,
+    OutlinedInput,
+    Select,
+    TextField,
+} from '@material-ui/core';
+
+const LibrarianAddBook = () => {
+
+    const history = useHistory();
+
+    const [author, setAuthor] = useState('');
+    const [date, setDate] = useState(new Date());
+    const today = new Date();
+
+    const [authors, setAuthors] = useState([]);
+
+    const handleAuthorChange = (event) => {
+        setAuthor(event.target.value);
+    };
+
+    const handleDateChange = (event) => {
+        setDate(event.target.value);
+    };
+
+    useEffect(() => {
+        fetchAuthors().then((data) => setAuthors(data.data));
+    }, []);
+
+    function createBook(data, helper) {
+        data.author = author;
+        console.log(data)
+        createBookApi(data).then((data) => {
+            history.push('book/' + data.data.id + '/copies');
+        });
+        helper.setSubmitting(false);
+    }
+
+    return (
+        <>
+            <Container style={{
+                minHeight: '70vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <Box>
+                    <Formik
+                        initialValues={{
+                            isbn: '',
+                            author: {},
+                            title: '',
+                            publishedAt: {},
+                        }}
+                        onSubmit={createBook}
+                    >
+                        {(props) => (
+                            <Card>
+                                <Form onSubmit={props.handleSubmit} >
+                                    <CardHeader
+                                        title="Add book"
+                                        titleTypographyProps={{
+                                            align: 'center',
+                                        }}
+                                        action={null}
+                                        subheaderTypographyProps={{
+                                            align: 'center',
+                                        }}
+                                        sx={{
+                                            backgroundColor: (
+                                                theme
+                                            ) =>
+                                                theme.palette
+                                                    .mode ===
+                                                'light'
+                                                    ? theme.palette
+                                                        .grey[200]
+                                                    : theme.palette
+                                                        .grey[700],
+                                        }}
+                                    />
+                                    <CardContent>
+                                            <Grid item xs={12}>
+                                                <FormControl
+                                                    fullWidth
+                                                    variant="outlined"
+                                                    margin="dense"
+                                                >
+                                                    <InputLabel htmlFor="isbn">
+                                                        ISBN
+                                                    </InputLabel>
+                                                    <Field
+                                                        required
+                                                        id="isbn"
+                                                        name="isbn"
+                                                        label="ISBN"
+                                                        as={
+                                                            OutlinedInput
+                                                        }
+                                                    />
+                                                </FormControl>
+                                                <FormControl
+                                                    fullWidth
+                                                    variant="outlined"
+                                                    margin="dense"
+                                                >
+                                                    <InputLabel htmlFor="title">
+                                                        Title
+                                                    </InputLabel>
+                                                    <Field
+                                                        required
+                                                        id="title"
+                                                        name="title"
+                                                        label="Title"
+                                                        as={
+                                                            OutlinedInput
+                                                        }
+                                                    />
+                                                </FormControl>
+                                                <FormControl
+                                                    fullWidth
+                                                    variant="outlined"
+                                                    margin="dense"
+                                                >
+                                                    <InputLabel
+                                                        id="author-label"
+                                                        htmlFor="author"
+                                                    >
+                                                        Author
+                                                    </InputLabel>
+                                                    <Select
+                                                        id="author"
+                                                        value={
+                                                            author
+                                                        }
+                                                        onChange={
+                                                            handleAuthorChange
+                                                        }
+                                                    >
+                                                        {authors.map(
+                                                            (v) => (
+                                                                <MenuItem
+                                                                    value={
+                                                                        v
+                                                                    }
+                                                                >
+                                                                    {
+                                                                        v.name
+                                                                    }
+                                                                </MenuItem>
+                                                            )
+                                                        )}
+                                                    </Select>
+                                                </FormControl>
+                                                <FormControl
+                                                    fullWidth
+                                                    variant="outlined"
+                                                    margin="dense"
+                                                >
+                                                    <Field
+                                                        id="publishedAt"
+                                                        name="publishedAt"
+                                                        label="Date of publish"
+                                                        type="date"
+                                                        defaultValue={new Date().getDate()}
+                                                        InputLabelProps={{
+                                                            shrink: true,
+                                                        }}
+                                                    />
+                                                </FormControl>
+                                            </Grid>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Box
+                                            sx={{
+                                                width: '100%',
+                                                display: 'flex',
+                                                justifyContent:
+                                                    'center',
+                                            }}
+                                        >
+                                            {props.isSubmitting ? (
+                                                <span>
+                                                                Submiting...
+                                                            </span>
+                                            ) : (
+                                                <PrimaryButton type="submit">
+                                                    Submit
+                                                </PrimaryButton>
+                                            )}
+                                        </Box>
+                                    </CardActions>
+                                </Form>
+                            </Card>
+                        )}
+                    </Formik>
+                </Box>
+            </Container>
+        </>
+    );
+};
+
+export default LibrarianAddBook;
---
+++ ui/src/containers/Pages/LibrarianPages/LibrarianUserSearch.js
@@ -0,0 +1,141 @@
+import {
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    Container,
+    FormControl,
+    Grid,
+    InputLabel,
+    MenuItem,
+    OutlinedInput,
+    Select
+} from "@material-ui/core";
+import {Field, Form, Formik} from "formik";
+import {PrimaryButton} from "../../../components/StyledItems";
+import DayPickerInput from "react-day-picker/DayPickerInput";
+import dateFnsFormat from "date-fns/format";
+import * as React from "react";
+import {useHistory} from "react-router-dom";
+
+
+const LibrarianUserSearch = () => {
+    const history = useHistory();
+
+    const searchByUserNumber = (data, helper) => {
+        helper.setSubmitting(false);
+        const userNumber = data.userNumber;
+        history.push('/user/' + userNumber + '/copies');
+    };
+
+    return (
+        <>
+            <Container>
+                <Box p={10}>
+                    <Grid
+                        container
+                        spacing={4}
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Grid item key={1} xs={12} sm={10} md={4}>
+                            <Formik
+                                initialValues={{
+                                    userNumber: '',
+                                }}
+                                onSubmit={searchByUserNumber}
+                            >
+                                {(props) => (
+                                    <div>
+                                        <Card>
+                                            <Form onSubmit={props.handleSubmit}>
+                                                <CardHeader
+                                                    title="Check user copies"
+                                                    subheader="Enter user number"
+                                                    titleTypographyProps={{
+                                                        align: 'center',
+                                                    }}
+                                                    action={null}
+                                                    subheaderTypographyProps={{
+                                                        align: 'center',
+                                                    }}
+                                                    sx={{
+                                                        backgroundColor: (
+                                                            theme
+                                                        ) =>
+                                                            theme.palette
+                                                                .mode ===
+                                                            'light'
+                                                                ? theme.palette
+                                                                    .grey[200]
+                                                                : theme.palette
+                                                                    .grey[700],
+                                                    }}
+                                                />
+                                                <CardContent>
+                                                    <Box
+                                                        sx={{
+                                                            width: '100%',
+                                                            display: 'flex',
+                                                            justifyContent:
+                                                                'center',
+                                                        }}
+                                                    >
+                                                        <Grid item xs={12}>
+                                                            <FormControl
+                                                                fullWidth
+                                                                variant="outlined"
+                                                                margin="dense"
+                                                            >
+                                                                <InputLabel htmlFor="userNumber">
+                                                                    User number
+                                                                </InputLabel>
+                                                                <Field
+                                                                    required
+                                                                    id="userNumber"
+                                                                    name="userNumber"
+                                                                    label="User number"
+                                                                    as={
+                                                                        OutlinedInput
+                                                                    }
+                                                                />
+                                                            </FormControl>
+                                                        </Grid>
+                                                    </Box>
+                                                </CardContent>
+                                                <CardActions>
+                                                    <Box
+                                                        sx={{
+                                                            width: '100%',
+                                                            display: 'flex',
+                                                            justifyContent:
+                                                                'center',
+                                                        }}
+                                                    >
+                                                        {props.isSubmitting ? (
+                                                            <span>
+                                                                Submiting...
+                                                            </span>
+                                                        ) : (
+                                                            <PrimaryButton type="submit">
+                                                                Search
+                                                            </PrimaryButton>
+                                                        )}
+                                                    </Box>
+                                                </CardActions>
+                                            </Form>
+                                        </Card>
+                                    </div>
+                                )}
+                            </Formik>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+        </>
+    );
+};
+
+
+export default LibrarianUserSearch;
